20a9a447ade57eb2290d1c6014e1c3727f0915c4
Jairo Calmon <heekinho@gmail.com>
2018-04-19 21:33:51 -0300

    Add missing include file guards

--- image_operations.h
+++ image_operations.h
@@ -1,3 +1,6 @@
+#ifndef IMAGE_OPERATIONS_H_
+#define IMAGE_OPERATIONS_H_
+
 #include "string.h"
 
 //Helper class style is the best here?
@@ -18,3 +21,6 @@ class ImageOperations {
      */
     static char* FlipHorizontal(const char *image, size_t cols, size_t rows);
 };
+
+
+#endif /* IMAGE_OPERATIONS_H_ */
